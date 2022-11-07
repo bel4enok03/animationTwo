dd8a4eb66dfeeeb7783b27910b602935db43e62e
bel4enok03 <belskaya-tanyush@mail.ru>
2022-11-07 17:15:40 +0400

    *

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-gsap.to('.image',{rotation:360, x:400,duration:3,delay:0.4 })
-gsap.to('.par',{x:300,duration:3,delay:1.5,opacity:1 })
+let colors = ["green","red","pink","blue",]
+gsap.to('.box',{rotation:230, x:400,duration:3,delay:0.4,stagger:1,backgroundColor:function(i){
+    return colors [i%4]
+} })
